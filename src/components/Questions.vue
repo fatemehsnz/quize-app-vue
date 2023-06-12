c2ff14a5fdd96f0a7d2c212850cb820eb507ff04
fatemeh hasanzadeh <fatemeh.hasanzadeh97@yahoo.com>
2023-06-12 15:15:02 +0330

    loop for questions and answers

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -2,15 +2,14 @@
   <div class="questions-ctr">
         <div class="progress">
             <div class="bar"></div>
-            <div class="status">1 out of 3 questions answered</div>
+            <div class="status">{{ questionsAnswered }} out of {{ questions.length }} questions answered</div>
         </div>
-        <div class="single-question">
-            <div class="question">Sample Question 1</div>
-            <div class="answers">
-                <div class="answer">Sample Answer 1</div>
-                <div class="answer">Sample Answer 2</div>
-                <div class="answer">Sample Answer 3</div>
-                <div class="answer">Sample Answer 4</div>
+        <div class="single-question" 
+            v-for="(question, index) in questions " :key="question.q" 
+            v-show="index === questionsAnswered">
+            <div class="question" >{{ question.q }}</div>
+            <div class="answers" v-for="answer in question.answers" :key="answer.text">
+                <div class="answer" >{{answer.text}}</div>
             </div>
         </div>
     </div>
@@ -18,7 +17,8 @@
 
 <script>
 export default {
-
+    props:['questions','questionsAnswered'],
+        
 }
 </script>
 
