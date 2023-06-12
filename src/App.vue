668195354ee5323418822fb9fa2ba70393eddb6d
fatemeh hasanzadeh <fatemeh.hasanzadeh97@yahoo.com>
2023-06-12 16:23:09 +0330

    add reset button and show appropriat result

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,14 @@
       :questionsAnswered=" questionsAnswered"
       @question-answered="questionAnswered"
     />
-    <results v-else></results>
-    <button type="button" class="reset-btn">Reset</button>
+    <results v-else 
+      :results="results"
+      :totalCorrect="totalCorrect"
+    />
+    <button type="button" class="reset-btn" 
+      @click.prevent="reset" 
+      v-show="questionsAnswered === questions.length"
+    >Reset</button>
 </div>
 </template>
 
@@ -107,6 +113,10 @@ export default {
         this.totalCorrect++;
       }
       this.questionsAnswered++;
+    },
+    reset(){
+      this.totalCorrect = 0
+      this.questionsAnswered = 0
     }
   }
 }
